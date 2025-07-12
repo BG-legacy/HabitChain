4429b843bc0d522d961ed76466fbab93dff1c62e
BG-legacy <bginnjr20@gmail.com>
2025-07-12 18:42:47 -0400

    add somethings

--- backend/HabitChain.Application/obj/Debug/net9.0/HabitChain.Application.AssemblyInfo.cs
+++ backend/HabitChain.Application/obj/Debug/net9.0/HabitChain.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HabitChain.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7635fea5c63834a077385eb4fb79891004b72838")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+078f13ca0320450c278aee1cd48d67f54a431ad4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HabitChain.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HabitChain.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
